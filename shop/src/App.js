4b4b5b3d1ddf0815ad3466d0266b58430a4c341c
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-09-01 00:12:58 +0200

    initial commit

---
+++ shop/src/App.js
@@ -0,0 +1,15 @@
+import React, {component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4 className="bg-primary text-white text-center p-2">
+        Lista zadań
+      </h4>
+    </div>
+  );
+}
+
+export default App;
